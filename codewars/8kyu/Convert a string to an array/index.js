992d4eed55f1a7e40c112d29c801145e9a587421
trush1n <trushin.i.n@gmail.com>
2024-10-06 21:19:38 +0300

    Convert a string to an array

---
+++ codewars/8kyu/Convert a string to an array/index.js
@@ -0,0 +1,9 @@
+function stringToArray(string) {
+
+    console.log(string.split(" "))
+
+}
+
+stringToArray("Robin Singh")//, ["Robin", "Singh"]);
+
+stringToArray("I love arrays they are my favorite")//, ["I", "love", "arrays", "they", "are", "my", "favorite"]);
