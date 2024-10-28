f0af598632feb43d70504446d30b72e7770e7134
trush1n <trushin.i.n@gmail.com>
2024-10-28 22:47:10 +0300

    8kyu

---
+++ codewars/8kyu/findThePosition!/index.js
@@ -0,0 +1,9 @@
+function position(letter){
+    //Write your own Code!
+      console.log("Position of alphabet: " + (letter.charCodeAt() - 96));
+    }
+
+    position("a")//,"Position of alphabet: 1");
+    position("z")//,"Position of alphabet: 26");
+    position("e")//,"Position of alphabet: 5");
+  
