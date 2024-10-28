3fa7d7106667a6775faa52adc1436a13e84303f9
trush1n <trushin.i.n@gmail.com>
2024-10-28 22:59:09 +0300

    8kyu

---
+++ codewars/8kyu/RemoveAllExclamationMarksFromTheEndOfSentence/index.js
@@ -0,0 +1,20 @@
+function remove(string) {
+    let i = 1;
+    let count = 0;
+    while (string[string.length - i] == "!") {
+      count += 1;
+      i++;
+    }
+    let noStringLast  = "";
+    for (let j = 0; j < string.length - count; j++) {
+      noStringLast += string[j];
+    }
+    console.log(noStringLast)
+  }
+
+  remove("Hi!")//, "Hi")
+  remove("Hi!!!")//, "Hi")
+  remove("!Hi")//, "!Hi")
+  remove("!Hi!")//, "!Hi")
+  remove("Hi! Hi!")//, "Hi! Hi")
+  remove("Hi")//, "Hi")
