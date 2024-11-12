303a33f9bead87561d191dc725de55036d698294
trush1n <trushin.i.n@gmail.com>
2024-11-13 00:45:28 +0300

    add if (prop in contacts[i]

--- freecodecamp/Legacy JavaScript Algorithms and Data Structures/Step 103 Profile Lookup/index.js
+++ freecodecamp/Legacy JavaScript Algorithms and Data Structures/Step 103 Profile Lookup/index.js
@@ -1,45 +1,49 @@
 // Setup
 const contacts = [
     {
-        firstName: "Akira",
-        lastName: "Laine",
-        number: "0543236543",
-        likes: ["Pizza", "Coding", "Brownie Points"],
+      firstName: "Akira",
+      lastName: "Laine",
+      number: "0543236543",
+      likes: ["Pizza", "Coding", "Brownie Points"],
     },
     {
-        firstName: "Harry",
-        lastName: "Potter",
-        number: "0994372684",
-        likes: ["Hogwarts", "Magic", "Hagrid"],
+      firstName: "Harry",
+      lastName: "Potter",
+      number: "0994372684",
+      likes: ["Hogwarts", "Magic", "Hagrid"],
     },
     {
-        firstName: "Sherlock",
-        lastName: "Holmes",
-        number: "0487345643",
-        likes: ["Intriguing Cases", "Violin"],
+      firstName: "Sherlock",
+      lastName: "Holmes",
+      number: "0487345643",
+      likes: ["Intriguing Cases", "Violin"],
     },
     {
-        firstName: "Kristian",
-        lastName: "Vos",
-        number: "unknown",
-        likes: ["JavaScript", "Gaming", "Foxes"],
+      firstName: "Kristian",
+      lastName: "Vos",
+      number: "unknown",
+      likes: ["JavaScript", "Gaming", "Foxes"],
     },
-];
-
-function lookUpProfile(name, prop) {
-    // Only change code below this line
-    for (let i = 0; i < contacts.length; i++) {
-        if (contacts[i].firstName === name) {
-            console.log(contacts[i][prop])
+  ];
+  
+  function lookUpProfile(name, prop) {
+      // Only change code below this line
+      for (let i = 0; i < contacts.length; i++) {
+      if (contacts[i].firstName === name) {
+        if (prop in contacts[i]) {
+          return contacts[i][prop];
         } else {
-            console.log("No such contact")
+          return "No such property";
         }
+      }
     }
-    // Only change code aove this line
-}
-//lookUpProfile("Bob", "number")//должен вернуть строкуNo such contact
-//lookUpProfile("Harry", "likes")//должен возвращать массив
-//lookUpProfile("Kristian", "lastName")
-//lookUpProfile("Sherlock", "likes")//должен вернуться["Intriguing Cases", "Violin"]
-//lookUpProfile("Akira", "likes");
+    return "No such contact";
+  }
+      // Only change code aove this line
+  
+  lookUpProfile("Akira", "likes");
+lookUpProfile("Bob", "number")//должен вернуть строкуNo such contact
+lookUpProfile("Harry", "likes")//должен возвращать массив
+lookUpProfile("Sherlock", "likes")//должен вернуться["Intriguing Cases", "Violin"]
+lookUpProfile("Akira", "likes");
 lookUpProfile("Kristian", "lastName")//должен вернуть строкуVos
