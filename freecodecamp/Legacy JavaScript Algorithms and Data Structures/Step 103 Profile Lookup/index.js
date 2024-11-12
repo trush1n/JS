197c3eefd11f4afec31f86936e5fc4e25da30eeb
trush1n <trushin.i.n@gmail.com>
2024-11-13 00:38:07 +0300

    Step 103 Profile Lookup

---
+++ freecodecamp/Legacy JavaScript Algorithms and Data Structures/Step 103 Profile Lookup/index.js
@@ -0,0 +1,45 @@
+// Setup
+const contacts = [
+    {
+        firstName: "Akira",
+        lastName: "Laine",
+        number: "0543236543",
+        likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+        firstName: "Harry",
+        lastName: "Potter",
+        number: "0994372684",
+        likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+        firstName: "Sherlock",
+        lastName: "Holmes",
+        number: "0487345643",
+        likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+        firstName: "Kristian",
+        lastName: "Vos",
+        number: "unknown",
+        likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+];
+
+function lookUpProfile(name, prop) {
+    // Only change code below this line
+    for (let i = 0; i < contacts.length; i++) {
+        if (contacts[i].firstName === name) {
+            console.log(contacts[i][prop])
+        } else {
+            console.log("No such contact")
+        }
+    }
+    // Only change code aove this line
+}
+//lookUpProfile("Bob", "number")//должен вернуть строкуNo such contact
+//lookUpProfile("Harry", "likes")//должен возвращать массив
+//lookUpProfile("Kristian", "lastName")
+//lookUpProfile("Sherlock", "likes")//должен вернуться["Intriguing Cases", "Violin"]
+//lookUpProfile("Akira", "likes");
+lookUpProfile("Kristian", "lastName")//должен вернуть строкуVos
