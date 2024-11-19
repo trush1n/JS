4065feeecf64eca991e07113a235a603c63c144b
trush1n <trushin.i.n@gmail.com>
2024-11-19 22:57:32 +0300

    Step 107 Use the parseInt Function

---
+++ freecodecamp/Legacy JavaScript Algorithms and Data Structures/Step 107 Use the parseInt Function/index.js
@@ -0,0 +1,8 @@
+function convertToInteger(str) {
+    return parseInt(str)
+  }
+  
+  convertToInteger("56")//должен вернуть число
+  convertToInteger("56")//должен вернуть 56
+  convertToInteger("77")//должен вернуть 77
+  convertToInteger("JamesBond")//должен вернутьсяNaN
