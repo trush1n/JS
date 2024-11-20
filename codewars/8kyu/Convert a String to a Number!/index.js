b0d758e3b2dec5165848e69d9b4564b5d34d6a20
trush1n <trushin.i.n@gmail.com>
2024-11-20 20:58:57 +0300

    Task 8 kyu Cinvert a String to a Number!

---
+++ codewars/8kyu/Convert a String to a Number!/index.js
@@ -0,0 +1,9 @@
+const stringToNumber = function(str){
+    // put your code here
+    return Number(str);
+  }
+
+stringToNumber("1234")//,1234)
+stringToNumber("605")//, 605)
+stringToNumber("1405")//,1405)
+stringToNumber("-7")//,  -7)
