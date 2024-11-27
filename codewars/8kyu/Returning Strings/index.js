15951e33bedfd60c2c2ea202f0d1fce51ff8f5bb
trush1n <trushin.i.n@gmail.com>
2024-11-27 21:17:00 +0300

    Task 8 kyu Returning Strings

---
+++ codewars/8kyu/Returning Strings/index.js
@@ -0,0 +1,4 @@
+const greet = name =>`Hello, ${name} how are you doing today?`;
+
+greet("Ryan")//, "Hello, Ryan how are you doing today?");
+greet("Shingles")//, "Hello, Shingles how are you doing today?");
