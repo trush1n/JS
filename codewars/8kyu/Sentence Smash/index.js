ceb08a1bf6eb9c1f526423f2087743fae8304477
trush1n <trushin.i.n@gmail.com>
2024-11-28 21:17:52 +0300

    Task 8 kyu Sentence Smash

---
+++ codewars/8kyu/Sentence Smash/index.js
@@ -0,0 +1,5 @@
+const smash = words => words.join(" ");
+
+smash(["hello", "world"])//, "hello world");
+smash(["hello", "amazing", "world"])//, "hello amazing world");
+smash(["this", "is", "a", "really", "long", "sentence"])//, "this is a really long sentence");
